12c6d0c34409aaced73c3641f25cf523abb577a7
Ashutosh Gupta <ashutoshgupta0316@gmail.com>
2026-06-16 03:08:26 +0530

    setup env and gitignore

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import { createServer } from "node:http";
 
@@ -22,18 +25,12 @@ app.use(express.urlencoded({ limit: "40kb", extended: true }));
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
-
-    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
+    const connectionDb = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`)
+    server.listen(process.env.PORT || 8000, () => {
+        console.log("LISTENING ON PORT 8000")
     });
-
-
-
 }
-
 
 
 start();
